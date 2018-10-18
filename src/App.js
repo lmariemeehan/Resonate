5a85ebedbd7bfe89e1df526829c2257bbe2d4c4f
Lola <lmariemeehan@gmail.com>
2018-10-17 21:41:11 -0700

    Checkpoint 5 Components Library completed

--- src/App.js
+++ src/App.js
@@ -12,18 +12,17 @@ class App extends Component {
        <header>
           <nav>
             <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
-              </nav>
-        <h1>Bloc Jams</h1>
+            <Link to='/library'>Library</Link>                                                  
+            </nav>        
+            <h1>Bloc Jams</h1>
        </header>
        <main>
-       <Route exact path="/" component={Landing} />
-       <Route path="/library" component={Library} />
-       <Route path="/album" component={Album} />
-       </main>
-      </div>
-    );
-  }
-}
-
-export default App;
+       <Route exact path="/" component={Landing} />       
+       <Route path="/library" component={Library} />       
+       <Route path="/album/:slug" component={Album} />       
+       </main>      
+       </div>    
+       );  
+     }
+     }
+       export default App;
