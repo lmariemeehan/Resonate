28ae85c36a3995cc4bfdfa00eae55bc05ddb8b74
Lola <lmariemeehan@gmail.com>
2018-10-22 15:06:24 -0700

    Assignment 6 Compoponents:Album completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -25,14 +25,27 @@ class Album extends Component {
              <div id="release-info">{this.state.album.releaseInfo}</div>
            </div>
        	  </section>
+
 	       	  <table id="song-list">
 	           <colgroup>
 	             <col id="song-number-column" />
 	             <col id="song-title-column" />
 	             <col id="song-duration-column" />
 	           </colgroup>  
+	           
+
 	           <tbody>
-	           </tbody>
+	             {
+	             	this.state.album.songs.map( (song, index) => 
+	             	<tr className="song" key={index}> 
+	             	<td className="song-number"> {index + 1} </td>
+	             	<td className="song-title"> {song.title} </td>
+	             	<td className="song-duration"> {song.duration} seconds </td>
+	             	</tr>		
+		            )
+		          }
+		       </tbody>
+	         
 	         </table>
        </section>
      );
