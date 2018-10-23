1155d42f712557835419ce7475e585b02387bde9
Lola <lmariemeehan@gmail.com>
2018-10-22 17:00:25 -0700

    Checkpoint 7 Audio Playback completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,8 +10,39 @@ class Album extends Component {
      });
  
      this.state = {
-       album: album
+       album: album,
+       currentSong: album.songs[0],
+       isPlaying: false
      };
+
+     this.audioElement = document.createElement('audio');
+     this.audioElement.src = album.songs[0].audioSrc;
+
+   }
+
+   play() {
+   	this.audioElement.play();
+   	this.setState({isPlaying: true});
+   }
+
+   pause() {
+   	this.audioElement.pause();
+   	this.setState({isPlaying: false});
+   }
+
+   setSong(song) {
+   	this.audioElement.src = song.audioSrc;
+   	this.setState({ currentSong: song});
+   }
+
+   handleSongClick(song) {
+   	const isSameSong = this.state.currentSong === song;
+   	if (this.state.isPlaying && isSameSong) {
+   		this.pause();
+   	} else {
+   		if (!isSameSong) { this.setSong(song);}
+   		this.play();
+   	}
    }
  
    render() {
@@ -37,7 +68,7 @@ class Album extends Component {
 	           <tbody>
 	             {
 	             	this.state.album.songs.map( (song, index) => 
-	             	<tr className="song" key={index}> 
+	             	<tr className="song" key={index} onClick= {() => this.handleSongClick(song)}> 
 	             	<td className="song-number"> {index + 1} </td>
 	             	<td className="song-title"> {song.title} </td>
 	             	<td className="song-duration"> {song.duration} seconds </td>
