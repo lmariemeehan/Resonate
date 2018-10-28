a8f3de229b825e28fd6299c3426c1142b4150ec5
Lola <lmariemeehan@gmail.com>
2018-10-28 00:04:51 -0700

    Completed assignment 8 player bar buttons

--- src/components/Album.js
+++ src/components/Album.js
@@ -56,6 +56,14 @@ class Album extends Component {
    	this.play();
    }
 
+   handleNextClick() {
+   	const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+   	const newIndex = Math.floor(currentIndex + 1);
+   	const newSong = this.state.album.songs[newIndex];
+   	this.setSong(newSong);
+   	this.play();
+   }
+
    onMouseEnter(index){
     this.setState({isHovered: index});
   }
@@ -110,7 +118,7 @@ class Album extends Component {
 		     </tbody>	
 
 	        </table>
-	        <PlayerBar isPlaying={this.state.isPlaying} currentSong={this.state.currentSong} handleSongClick={() => this.handleSongClick(this.state.currentSong)} handlePrevClick={() => this.handlePrevClick()}/>
+	        <PlayerBar isPlaying={this.state.isPlaying} currentSong={this.state.currentSong} handleSongClick={() => this.handleSongClick(this.state.currentSong)} handlePrevClick={() => this.handlePrevClick()} handleNextClick={() => this.handleNextClick()}/>
        </section>
      );
    }
