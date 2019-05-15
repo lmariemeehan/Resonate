8ce3641151e90bf60bcaa317b41d2491fcaf416e
Lola <lmariemeehan@gmail.com>
2019-05-14 23:53:36 -0700

    Added images to library

--- src/data/albums.js
+++ src/data/albums.js
@@ -12,16 +12,68 @@ export default [{
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
   ]
 }, {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    releaseInfo: '1909 EM Records',
-    albumCover: '/assets/images/album_covers/02.jpg',
-    slug: 'the-telephone',
-    songs: [
+  title: 'Tree At Sunset',
+  artist: 'Bessi',
+  releaseInfo: '2012 Travel Records',
+  albumCover: '/assets/images/album_covers/treeAtSunset.jpg',
+  slug: 'tree-at-sunset',
+  songs: [
       { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
       { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
       { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
       { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
-    ]
+  ]
+}, {
+  title: 'Night',
+  artist: 'Skeeze',
+  releaseInfo: '2013 Delicate Arch Records',
+  albumCover: '/assets/images/album_covers/night.jpg',
+  slug: 'night',
+  songs: [
+      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+  ]
+}, {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  releaseInfo: '1909 EM Records',
+  albumCover: '/assets/images/album_covers/02.jpg',
+  slug: 'the-telephone',
+  songs: [
+    { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+    { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+    { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+    { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+    { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+  ]
+}, {
+  title: 'Wise Owl',
+  artist: 'Lepale',
+  releaseInfo: '2016 Bird Records',
+  albumCover: '/assets/images/album_covers/owl.jpg',
+  slug: 'wise-owl',
+  songs: [
+    { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+    { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+    { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+    { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+    { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+  ]
+}, {
+  title: 'The Bellyflop',
+  artist: 'Guy',
+  releaseInfo: '2012 Swim Records',
+  albumCover: '/assets/images/album_covers/03.jpg',
+  slug: 'the-bellyflop',
+  songs: [
+      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+  ]
 }];
