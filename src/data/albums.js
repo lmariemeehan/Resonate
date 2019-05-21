5487be5ac3364e424bfefa2e329e011b8fef6bfa
Lola <lmariemeehan@gmail.com>
2019-05-20 23:59:47 -0700

    Taking away carousel

--- src/data/albums.js
+++ src/data/albums.js
@@ -129,6 +129,19 @@ export default [{
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
   ]
 }, {
+  title: 'Favorite Spot',
+  artist: 'enriquelopezgarre',
+  releaseInfo: '2019 Gateway Records',
+  albumCover: '/assets/images/album_covers/landscape-4205108_640.jpg',
+  slug: 'favorite-spot',
+  songs: [
+      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+  ]
+}, {
   title: 'Thunderstorm',
   artist: 'O12',
   releaseInfo: '2018 Flash Records',
