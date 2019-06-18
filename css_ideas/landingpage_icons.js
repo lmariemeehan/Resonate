9ab29b1a62d2a8a9e257cace7986819451f56c6f
Lola <lmariemeehan@gmail.com>
2019-06-18 01:21:53 -0700

    Added images to selling points

---
+++ css_ideas/landingpage_icons.js
@@ -0,0 +1,21 @@
+<div className="sellingpoints-container">
+
+  <div className="selling-point" >
+    <span className="music-note"><ion-icon name="ios-musical-notes"></ion-icon></span>
+    <h3 className="point-title">Choose your music</h3>
+    <p className="point-description">The world is full of music. Why should you have to listen to music that someone else chose?</p>
+  </div>
+
+  <div className="selling-point" >
+    <span className="streaming-icon"><ion-icon name="ios-radio"></ion-icon></span>
+    <h3 className="point-title">Unlimited, streaming, ad-free</h3>
+    <p className="point-description">No arbitrary limits. No distractions.</p>
+  </div>
+
+  <div className="selling-point" >
+    <span className="phone-icon"><ion-icon name="phone-portrait"></ion-icon></span>
+    <h3 className="point-title">Mobile enabled</h3>
+    <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+  </div>
+
+</div>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,28 +7,38 @@ const Landing = () => (
   <section className="landing">
 
     <div className="landing_page">
-      <h4 className="hero-title"><a href="/library">Turn the music <span>UP!</span></a></h4>
+      <h1 className="hero-title"><a href="/library">Turn the music <span>UP!</span></a></h1>
     </div>
 
     {/*Sellings Points*/}
+
       <div className="sellingpoints-container">
 
-        <div className="selling-point" >
-          <span className="music-note"><ion-icon name="ios-musical-notes"></ion-icon></span>
-          <h2 className="point-title">Choose your music</h2>
-          <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
+        <div className="selling-point-1" >
+          <img src="/assets/images/landing_page/vinyl-records-945396_1920.jpg" width="500" height="400" alt="" />
+          <div className="selling-descriptions">
+            <span className="music-note"><ion-icon name="ios-musical-notes"></ion-icon></span>
+            <h3 className="point-title">Choose your music</h3>
+            <p className="point-description">The world is full of music. Why should you have to listen to music that someone else chose?</p>
+          </div>
         </div>
 
-        <div className="selling-point" >
-          <span className="streaming-icon"><ion-icon name="ios-radio"></ion-icon></span>
-          <h2 className="point-title">Unlimited, streaming, ad-free</h2>
-          <p className="point-description">No arbitrary limits. No distractions.</p>
+        <div className="selling-point-2" >
+          <div className="selling-descriptions">
+            <span className="streaming-icon"><ion-icon name="ios-radio"></ion-icon></span>
+            <h3 className="point-title">Unlimited, streaming, ad-free</h3>
+            <p className="point-description">No arbitrary limits. No distractions.</p>
+          </div>
+          <img src="/assets/images/landing_page/bodybuilder-925770_1920.jpg" width="500" height="400" alt="" />
         </div>
 
-        <div className="selling-point" >
-          <span className="phone-icon"><ion-icon name="phone-portrait"></ion-icon></span>
-          <h2 className="point-title">Mobile enabled</h2>
-          <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+        <div className="selling-point-3" >
+          <img src="/assets/images/landing_page/mobile_adults.jpg" width="500" height="400" alt="" />
+          <div className="selling-descriptions">
+            <span className="phone-icon"><ion-icon name="phone-portrait"></ion-icon></span>
+            <h3 className="point-title">Mobile enabled</h3>
+            <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+          </div>
         </div>
 
       </div>
