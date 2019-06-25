92e99b02a1b0663466892ed3467c93bf8bb7c2a8
Lola <lmariemeehan@gmail.com>
2019-06-25 12:54:55 -0700

    Switched to bootstrap card deck instead for simplicity

--- css_ideas/bootstrap_masonry_landingpage.js
+++ css_ideas/bootstrap_masonry_landingpage.js
@@ -13,21 +13,29 @@ const Landing = () => (
     <h2>Resonate.</h2>
 
     <div className="sellingpoints-container">
-      <div className="card-deck">
+      <div className="card-columns">
         <div className="card">
           <img src="/assets/images/landing_page/vinyl-records-945396_1920.jpg" className="card-img-top" alt="..." />
           <div className="card-body">
             <h4><span className="music-note"><ion-icon name="ios-musical-notes"></ion-icon></span></h4>
             <h5 className="card-title">Choose your music</h5>
-            <p className="card-text">The world is full of music. Why should you have to listen to music that someone else chose?</p>
           </div>
         </div>
+          <div className="card text-center">
+            <div className="card-body-music-choices">
+              <p className="card-text">The world is full of music. Why should you have to listen to music that someone else chose?</p>
+            </div>
+          </div>
+          <div className="card text-center">
+            <div className="card-body-streaming">
+              <p className="card-text">No arbitrary limits. No distractions.</p>
+            </div>
+          </div>
         <div className="card">
           <img src="/assets/images/landing_page/bodybuilder-925770_1920.jpg" className="card-img-top" alt="..." />
           <div className="card-body">
             <h4><span className="streaming-icon"><ion-icon name="ios-radio"></ion-icon></span></h4>
             <h5 className="card-title">Unlimited, streaming, ad-free</h5>
-            <p className="card-text">No arbitrary limits. No distractions.</p>
           </div>
         </div>
         <div className="card">
@@ -35,6 +43,10 @@ const Landing = () => (
           <div className="card-body">
             <h4><span className="phone-icon"><ion-icon name="phone-portrait"></ion-icon></span></h4>
             <h5 className="card-title">Mobile enabled</h5>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className="card-body-mobile">
             <p className="card-text">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
           </div>
         </div>
