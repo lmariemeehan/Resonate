fc36826d301fd7e37c24f761a0f953ce4052c673
Lola <lmariemeehan@gmail.com>
2019-07-07 00:14:04 -0700

    Added some style to playerbar ranges

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -53,7 +53,7 @@ class PlayerBar extends Component {
 						   step="0.01"
 						   onChange={this.props.handleVolumeChange}
 					/>
-					<div className="icon ion-volume-high"></div>
+					<div className="icon ion-volume-high">{this.props.volume}</div>
 				</section>
 
 			</section>
