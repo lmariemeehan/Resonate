9575e2ab52604e9103230cecd9cc8e99cec86c33
Lola <lmariemeehan@gmail.com>
2020-07-16 13:42:19 -0700

    Got rid of unnecessary link import from app.js page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
